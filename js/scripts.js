abddceb826055e016ef6cae9e7cc2bf7e3ff022e
dcuthell <dcc1447@gmail.com>
2017-07-14 09:40:51 -0700

    add function to count numbers up to input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+function ppFunction(userInput){
+  var output = "";
+  var count = 0;
+  if(isNaN(userInput)){
+    alert("Please enter in a valid number");
+    return output;
+  }
+  count = parseInt(userInput);
+  for(var i = 0; i <= count; i++){
+    output += i + " ";
+  }
+  return output;
+}
+
 
 $(document).ready(function(){
-  $("#trackForm").submit(function(event){
-    
+  $("#inputForm").submit(function(event){
+    var input = $("input#inputNum").val();
+    var output = ppFunction(input);
+    $("#outputLine").text(output);
     event.preventDefault();
   });
 });
