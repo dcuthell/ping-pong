175923e73e4188c861582f27794d0611d34f6c3c
dcuthell <dcc1447@gmail.com>
2017-07-14 12:56:51 -0700

    add sizing for input field and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,11 @@
+//Back-End
 function clickPong(){
   var userInput = $("input#inputNum").val();
-  var output = "";
-  var count = 0;
-  if(isNaN(userInput)){
-    alert("Please enter in a valid number");
-    return output;
-  }
-  count = parseInt(userInput);
-  for(var i = count; i > 0; i--){
-    if(i%15 === 0){
-      output += "<li>ping-pong</li>";
-    }else if(i%5 === 0){
-      output += "<li>pong</li>";
-    }else if(i%3 === 0){
-      output += "<li>ping</li>";
-    }else{
-      output += "<li>" + i + "</li>";
-    }
-
-  }
+  var output = ppFunction(userInput, "pongping");
   $("#outputList").html(output);
 }
 
-function ppFunction(userInput){
+function ppFunction(userInput, ppStyle){
   var output = "";
   var count = 0;
   if(isNaN(userInput)){
@@ -30,26 +13,40 @@ function ppFunction(userInput){
     return output;
   }
   count = parseInt(userInput);
-  for(var i = 1; i <= count; i++){
-    if(i%15 === 0){
-      output += "<li>ping-pong</li>";
-    }else if(i%5 === 0){
-      output += "<li>pong</li>";
-    }else if(i%3 === 0){
-      output += "<li>ping</li>";
-    }else{
-      output += "<li>" + i + "</li>";
+  if(ppStyle === "pingpong"){
+    for(var i = 1; i <= count; i++){
+      if(i%15 === 0){
+        output += "<li>ping-pong</li>";
+      }else if(i%5 === 0){
+        output += "<li>pong</li>";
+      }else if(i%3 === 0){
+        output += "<li>ping</li>";
+      }else{
+        output += "<li>" + i + "</li>";
+      }
     }
-
+  } else if (ppStyle === "pongping"){
+    for(var i = count; i > 0; i--){
+      if(i%15 === 0){
+        output += "<li>ping-pong</li>";
+      }else if(i%5 === 0){
+        output += "<li>pong</li>";
+      }else if(i%3 === 0){
+        output += "<li>ping</li>";
+      }else{
+        output += "<li>" + i + "</li>";
+      }
+    }
+  }else{
+    alert("Invalid style type");
   }
   return output;
 }
 
-
 $(document).ready(function(){
   $("#inputForm").submit(function(event){
     var input = $("input#inputNum").val();
-    var output = ppFunction(input);
+    var output = ppFunction(input, "pingpong");
     $("#outputList").html(output);
     event.preventDefault();
   });
